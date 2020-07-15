107521feb655a5f9e6aebe4f84600865772db2bd
Rohit Sehgal <rohitsehgal1994@gmail.com>
2020-07-15 05:10:54 +0000

    Step 2 to separate TrashEmail

---
+++ src/test/java/io/github/trashemailtelegramconnector/TrashEmailTelegramConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.trashemailtelegramconnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrashEmailTelegramConnectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
